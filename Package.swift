72e7aefc051df3816f5b34645c93e6dcabb20c1f
Brian Strobach <brian@appsaurus.io>
2019-05-01 19:30:58 -0500

    Swift 5 tests passing on Linux and macOS

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -12,11 +12,11 @@ let package = Package(
     ],
     dependencies: [
 		.package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
-		.package(url: "https://github.com/vapor/fluent.git", .upToNextMajor(from:"3.0.0-rc.3.0.1")),
-		.package(url: "https://github.com/vapor/fluent-sqlite.git", .upToNextMajor(from:"3.0.0-rc.3.0.1")),
-		.package(url: "https://github.com/Appsaurus/FluentTestModels", .upToNextMajor(from: "0.1.0")),
-		.package(url: "https://github.com/Appsaurus/FluentTestUtils", .upToNextMajor(from: "0.1.0")),
-		.package(url: "https://github.com/Appsaurus/FluentSeeder", .upToNextMajor(from: "0.1.0")),
+		.package(url: "https://github.com/vapor/fluent.git", from:"3.0.0"),
+		.package(url: "https://github.com/vapor/fluent-sqlite.git", from:"3.0.0"),
+		.package(url: "https://github.com/Appsaurus/FluentTestModels", from: "0.1.0"),
+		.package(url: "https://github.com/Appsaurus/FluentTestUtils", from: "0.1.0"),
+		.package(url: "https://github.com/Appsaurus/FluentSeeder", from: "0.1.0"),
     ],
     targets: [
         .target(
