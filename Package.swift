438736a66cf02944d47439beb6a7ec2c4f50bfc9
Brian Strobach <brian@appsaurus.io>
2019-06-06 10:08:49 -0500

    Bumping deployment target

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "FluentTestApp",
+    platforms: [
+        .macOS(.v10_12)
+    ],
     products: [
         .library(
             name: "FluentTestApp",
